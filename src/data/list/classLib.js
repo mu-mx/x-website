ae0e1421dc4e11ab75392d3b0f3270a02be24de3
ext.xuhao16 <ext.xuhao16@jd.com>
2022-10-28 18:01:00 +0800

    类库

--- src/data/list/classLib.js
+++ src/data/list/classLib.js
@@ -1,11 +1,24 @@
 // 类库
-const _cdn =
-  "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-39734fbc-b241-4d89-ad87-0befd655e266/";
 export const classLib = {
   title: "类库",
   data: [
     {
-      src: _cdn + "1a45b478-83a3-4f8d-b04e-d038d01b0b32.png",
+      src: "https://ahooks.gitee.io/simple-logo.svg",
+      text: "ahooks",
+      url: "https://ahooks.gitee.io/zh-CN",
+    },
+    {
+      text: "formilyjs",
+      url: "https://v2.formilyjs.org/zh-CN",
+      title: "阿里巴巴统一前端表单解决方案",
+    },
+    {
+      text: "procomponents",
+      url: "https://procomponents.ant.design/",
+      title: "让中后台开发更简单",
+    },
+
+    {
       text: "axios",
       url: "http://www.axios-js.com/",
       title: "易用、简洁且高效的http库",
@@ -27,12 +40,15 @@ export const classLib = {
       url: "https://zeptojs.bootcss.com/#",
     },
     {
-      src: _cdn + "ad061805-85ad-4c4a-bf3e-97310384f41b.ico",
       text: "jq22",
       url: "https://www.jq22.com/",
     },
     {
-      src: _cdn + "08972837-21b6-41fa-b6c8-b09acfc36c12.png",
+      src: "https://dayjs.fenxianglu.cn/assets/favicon.png",
+      text: "dayjs",
+      url: "https://dayjs.fenxianglu.cn/",
+    },
+    {
       text: "Moment",
       url: "http://momentjs.cn/",
     },
@@ -42,21 +58,14 @@ export const classLib = {
       url: "https://v3.bootcss.com/",
     },
     {
-      src: _cdn + "c02b71d8-d851-4649-b023-a095c172fa72.png",
       text: "swiper",
       url: "https://3.swiper.com.cn/",
       title: "开源、免费、强大的移动端触摸滑动插件",
     },
     {
-      src: _cdn + "ec42ab2c-6526-4deb-b5d3-0894d2887fc0.png",
       text: "mockjs",
       url: "http://mockjs.com/",
       title: "生成随机数据，拦截 Ajax 请求",
-    },
-    {
-      src: "https://dayjs.fenxianglu.cn/assets/favicon.png",
-      text: "dayjs",
-      url: "https://dayjs.fenxianglu.cn/",
     },
     {
       src: "https://bennettfeely.com/ztext/img/icon/favicon-32x32.png",
@@ -74,7 +83,6 @@ export const classLib = {
       url: "https://pqina.nl/flip/#nutshell",
     },
     {
-      src: _cdn + "2a20ea40-eded-411d-a40d-ae45cd564f36.png",
       text: "notyf",
       url: "https://carlosroso.com/notyf/",
       title:
@@ -85,22 +93,13 @@ export const classLib = {
       text: "ejs",
       url: "https://ejs.bootcss.com/",
     },
-    {
-      src: "https://ahooks.gitee.io/simple-logo.svg",
-      text: "ahooks",
-      url: "https://ahooks.gitee.io/zh-CN",
-    },
+
     {
       src: "https://printjs.crabbly.com/assets/favicon.ico",
       text: "printjs",
       url: "https://printjs.crabbly.com/",
     },
-    {
-      src: _cdn + "66335525-89a1-4cd1-a5e0-035d6e4120c7.png",
-      text: "formilyjs",
-      url: "https://v2.formilyjs.org/zh-CN",
-      title: "阿里巴巴统一前端表单解决方案",
-    },
+
     {
       src: "https://www.tailwindcss.cn/favicon-32x32.png",
       text: "tailwindcss",
