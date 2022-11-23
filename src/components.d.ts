d0c1fed7dd57be056b97a19bec014d857fe074a5
ext.xuhao16 <ext.xuhao16@jd.com>
2022-11-23 16:09:50 +0800

    time

--- src/components.d.ts
+++ src/components.d.ts
@@ -15,7 +15,6 @@ declare module '@vue/runtime-core' {
     ElCollapse: typeof import('element-plus/es')['ElCollapse']
     ElCollapseItem: typeof import('element-plus/es')['ElCollapseItem']
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
-    ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
     ElDialog: typeof import('element-plus/es')['ElDialog']
     ElDivider: typeof import('element-plus/es')['ElDivider']
     ElForm: typeof import('element-plus/es')['ElForm']
