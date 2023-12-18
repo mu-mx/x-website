658debe606d93be3393246a4b16b07af75f25ae9
mu-mx <1017182993@qq.com>
2023-12-18 23:42:50 +0800

    请求数据

--- apps/next-x-web/src/app/page.tsx
+++ apps/next-x-web/src/app/page.tsx
@@ -4,26 +4,22 @@ import Link from 'next/link';
 import { Button } from 'antd';
 import { useEffect } from 'react';
 
+import Home from '@/app/views/Home';
+
 import useSWR from 'swr';
+
 
 const fetcher = (...args: Parameters<typeof fetch>) =>
   fetch(...args).then((res) => res.json());
 
+
 export default function Page() {
   const { data, error, isLoading } = useSWR('/api/website', fetcher);
-  console.log('data - >:', data)
+  console.log('data - >:', data);
 
   return (
     <>
-      <Button
-        type="primary"
-        onClick={() => {
-          console.log('2 - >:', 2);
-          console.log('data - >:', data);
-        }}
-      >
-        Button
-      </Button>
+      <Home />
     </>
   );
 }
---
+++ apps/next-x-web/src/app/views/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '@/app/components/Nav';
+import Search from '@/app/components/Search';
+
+export default function Index() {
+  return (
+    <>
+      <Search />
+
+      <Nav />
+    </>
+  );
+}
