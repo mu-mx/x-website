3b63f0f0772d7339a01dcb40a58e132edb4d8167
mu-mx <1017182993@qq.com>
2023-12-21 16:32:15 +0800

    api

--- apps/x-admin/src/api/index.ts
+++ apps/x-admin/src/api/index.ts
@@ -1,7 +1,7 @@
 import { request, useRequest } from "umi";
 
-// export const baseUrl = process.env.UMI_ENV === "dev" ? "/api" : process.env.BASE_API_URL;
-export const baseUrl = `http://a.com/api`;
+export const baseUrl =
+    process.env.UMI_ENV === "dev" ? `http://a.com/api` : "https://xw8-eta.vercel.app/";
 
 export const userAuth = async () => {
     return await request<any>(baseUrl + "/auth", {
