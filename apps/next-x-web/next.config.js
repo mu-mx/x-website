50b48c14831bdccef455c20e2cb4ad546d624238
mu-mx <1017182993@qq.com>
2024-01-26 20:49:12 +0800

    移除非路由

--- apps/next-x-web/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // output: 'export',
-  productionBrowserSourceMaps: true,
-  reactStrictMode: false,
-};
-
-module.exports = nextConfig;
--- apps/x-admin/global.d.ts
+++
@@ -1,8 +0,0 @@
-
-declare namespace NodeJS {
-  interface ProcessEnv {
-    readonly NODE_ENV: 'development' | 'production'
-    readonly BASE_API_URL: string
-    readonly GLOBAL_VAR: string
-  }
-}
